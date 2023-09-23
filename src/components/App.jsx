9197dd653bf933b6fd2e20c6114cd6097e5b3aab
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-09-23 17:08:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,16 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { addContact,deleteContact} from '../redux/contactSlice'; 
-import { filterSelector, setFilter } from './redux/filterSlice';
-import ContactForm from './components/ContactForm/ContactForm';
-import Filter from './components/Filter/Filter';
-import ContactList from './components/ContactList/ContactList';
-import css from './app.module.css';
+import ContactList from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import css from './app.module.css'
 
-const App = () => {
-  const dispatch = useDispatch(); 
-  const filter = useSelector(filterSelector); 
-  const handleFilterChange = (event) => {
-    dispatch(setFilter(event.target.value));
-  };
-
+export const App = () => {
   return (
     <div className={css.appContainer}>
-      <h1>Телефонная книга</h1>
-      <ContactForm onSubmit={(contact) => dispatch(addContact(contact))} />
-      <h2 className={css.h2Style}>Контакты</h2>
-      <Filter value={filter} onChangeFilter={handleFilterChange} />
-      <ContactList contacts={getViewContacts()} onDelete={deleteContact} /> 
+      <ContactForm />
+      <h2 className={css.h2Style}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
-}
-
+}; 
 export default App;
